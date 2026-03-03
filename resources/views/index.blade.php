457517706796cfbddd91fc72a4d995f8a1dca483
Cuauh_05 <cuaki.g.a@gmail.com>
2026-03-02 20:39:43 -0500

    tarea 5

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Products</title>
+</head>
+<body>
+
+    <h1>Product List</h1>
+
+    <a href="{{ route('products.create') }}">
+        <button>Add Product</button>
+    </a>
+
+</body>
+</html>
